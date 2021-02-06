6ecc972e159ce866cde6cddaa71aa2fd89657730
Alex <alexanderperreira@gmail.com>
2021-02-06 07:52:46 -0700

    contact submit fix with AJAX

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,3 +141,16 @@
 
 })(jQuery);
 
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('pizzaOrder');
+  let formData = new FormData(myForm);
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => console.log('Form successfully submitted.')).catch((error) => alert(error));
+}
+
+document.querySelector('form').addEventListenter('submit', handleSubmit);
